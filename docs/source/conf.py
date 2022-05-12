78aa703ca4f1382b225bb26931049a6ebab6fad2
Li Tianbo <li_tianbo@live.com>
2022-05-12 16:49:06 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Differantiable DFT'
+copyright = '2022, SEA AI LAB'
+author = 'Tianbo Li'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
